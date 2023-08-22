9babcc43ad12b350a04529fee8d3da809f5cd237
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-08-22 09:25:30 -0500

    Add files via upload

---
+++ Nuevos Programas/quintoPrograma.py
@@ -0,0 +1,27 @@
+precio_cafe = 1.50
+cantidad_de_cafes = 4
+ 
+# Prints "6.0"
+print(precio_cafe * cantidad_de_cafes)
+# Prints "1.5"
+print(precio_cafe)
+# Prints "4"
+print(cantidad_de_cafes)
+ 
+# Updating the price 
+precio_cafe = 2.00
+ 
+# Prints "8.0"
+print(precio_cafe * cantidad_de_cafes)
+# Prints "2.0"
+print(precio_cafe)
+# Prints "4"
+print(cantidad_de_cafes)
+
+precio_cafe = 2.5
+cantidad_de_cafes = 3
+
+print("Su cuenta a pagar es de: ",precio_cafe*cantidad_de_cafes)
+
+propina=1
+print("Su cuenta a pagar es de: ",(precio_cafe*cantidad_de_cafes)+propina)
