4fa289e364e7b8ff01a32c79dfa7662b85c67320
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-08-22 09:12:12 -0500

    Add files via upload

---
+++ Nuevos Programas/tercerPrograma.py
@@ -0,0 +1,19 @@
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal1 = "panquecas"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal1)
+
+# Now update meal to be lunch!
+meal2 = "Arroz con huevo"
+
+# Printing out lunch
+print("Lunch:")
+print(meal2)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner:")
+print(meal1)
