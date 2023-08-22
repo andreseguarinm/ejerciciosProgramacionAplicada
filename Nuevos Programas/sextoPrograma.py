df7b95ee27ba111d775ee94340c5245cfd0f7141
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-08-22 09:30:22 -0500

    Add files via upload

---
+++ Nuevos Programas/sextoPrograma.py
@@ -0,0 +1,16 @@
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+ 
+# 8 squared, or 64
+print(8 ** 2)
+ 
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 ** 3)
+ 
+# We can even perform fractional exponents
+# 4 to the half power, or 2
+print(4 ** 0.5)
+
+print("La raiz cuadrada de 7 es:",7**0.5)
+print("el cuadrado de 16 es: ", 16**2)
+print("La raiz cubica de 8 es: ",8**(1/3))
