cd7caa5e1afff07c71a806f50d5bca472c796207
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-08-22 09:37:02 -0500

    Add files via upload

---
+++ Nuevos Programas/septimoPrograma.py
@@ -0,0 +1,11 @@
+# Muestra 4 porque 29 / 5 es 5 con un residuo de 4
+print(29 % 5)
+ 
+# Muestra 2 porque 32 / 3 is 10 con un residuo de 2
+print(32 % 3)
+ 
+# Módulo por 2 devuelve 0 para números pares y 1 para números impares
+# Muestra 0
+print(44 % 2)
+
+print("el residuo de 35/3 es de: ", 35%3)
