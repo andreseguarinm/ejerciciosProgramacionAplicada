a3718fa437079f4178732045a65e46c455ff60af
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-09-10 13:18:52 -0500

    Add files via upload

---
+++ Loops/BucleWhile.py
@@ -0,0 +1,6 @@
+for i in range(1,6):
+    while i <= 4:
+        i += 1
+        print(i)
+    break    
+#Este programa imprime los números del 2 al 5, ya que dentro del bucle while, antes de imprimir el número, le suma una unidad.
