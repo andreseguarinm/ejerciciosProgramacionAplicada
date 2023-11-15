d1c4e1d633d52de00cb9dec1231e03e544550088
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-11-14 23:26:13 -0500

    Update Getting_Started.py

--- SegundoCorte/Numpy/Getting_Started.py
+++ SegundoCorte/Numpy/Getting_Started.py
@@ -1,1 +1,29 @@
+#Si ya tienes Python y PIP instalados en un sistema, entonces la instalación de NumPy es muy fácil.
+#Instálalo usando este comando: C:\Users\Your Name>pip install numpy
+
+#Una vez instalado Numpy, ya podemos importarla en nuestras aplicaciones usando el siguiente comando:
+
+import numpy
+
+#Ahora que ya fue importado, podemos darle utilidad, por ejemplo:
+
+import numpy
+arr = numpy.array([1, 2, 3, 4, 5]) #Matriz numérica
+print(arr)
+
+#Numpy también permite llamarlo con alias, y usualmente se usa 'np' para llamarlo:
+
+import numpy as np
+
+#Por ejemplo
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+#La versión de cadena se almacena en el atributo __version__:
+
+import numpy as np
+print(np.__version__)
+
 
