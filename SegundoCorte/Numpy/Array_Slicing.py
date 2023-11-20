a7db92d8ca6c7878fd37cfb37eec4f782fd41bec
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-11-19 22:48:03 -0500

    Update Array_Slicing.py

--- SegundoCorte/Numpy/Array_Slicing.py
+++ SegundoCorte/Numpy/Array_Slicing.py
@@ -7,33 +7,27 @@
 #Por ejemplo:
 
 import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
-print(arr[1:5]) #Solamente mostrará los números que se encuentran entre la segunda posición y la sexta posición; o sea 2, 3, 4 y 5.
-print(arr[4:]) #Mostrará los números desde la quinta posición hasta la última posición, o sea 5, 6 y 7
+arr1 = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr1[1:5]) #Solamente mostrará los números que se encuentran entre la segunda posición y la sexta posición; o sea 2, 3, 4 y 5.
+print(arr1[4:]) #Mostrará los números desde la quinta posición hasta la última posición, o sea 5, 6 y 7
 
 ##REBANADO NEGATIVO##
 #Se puede utilizar la lógica inversa para tomar una serie de elementos.
 #Por ejemplo:
 
-import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
-print(arr[-3:-1])
+print(arr1[-3:-1])
 
 ##STEP##
 #Permite mostrar todos los valores entre el rango escogido, excepto el valor de la posición indicada.
 #Por ejemplo:
-
-import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
-print(arr[1:5:2]) #Muestra todos los números desde la segunda posición hasta la sexta posición excepto el elemento de la tercera posición.
-print(arr[::2]) #Muestra todos los elementos excepto el de la tercera posición.
+print(arr1[1:5:2]) #Muestra todos los números desde la segunda posición hasta la sexta posición excepto el elemento de la tercera posición.
+print(arr1[::2]) #Muestra todos los elementos excepto el de la tercera posición.
 
 ##REBANAR MATRICES BIDIMENSIONALES##
 #Se puede rebanar matrices de dos dimensiones, en el cual se escoge cuál de las dimensiones se quiere rebanar y en qué rango se quiere rebanar.
 #Por ejemplo:
 
-import numpy as np
-arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(arr[1, 1:4]) #De la primera matriz, muestra los elementos desde la segunda posición hasta la quinta.
-print(arr[0:2, 2]) #De ambas matrices, sólo muestra los elementos de la tercera posición.
-print(arr[0:2, 1:4]) #De ambas matrices, muestra los elementos desde la segunda posición hasta la quinta.
+arr2 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr2[1, 1:4]) #De la primera matriz, muestra los elementos desde la segunda posición hasta la quinta.
+print(arr2[0:2, 2]) #De ambas matrices, sólo muestra los elementos de la tercera posición.
+print(arr2[0:2, 1:4]) #De ambas matrices, muestra los elementos desde la segunda posición hasta la quinta.
