03cc9c3d9625cd160c6abb7ca9668070e03c16c9
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-11-19 22:38:48 -0500

    Update Data_Types.py

--- SegundoCorte/Numpy/Data_Types.py
+++ SegundoCorte/Numpy/Data_Types.py
@@ -1,1 +1,9 @@
+#Por defecto, Python tiene estos tipos de datos:
 
+#strings- se utiliza para representar datos de texto, el texto aparece entre comillas. por ejemplo "ABCD"
+#integer- Se utiliza para representar números enteros. por ejemplo -1, -2, -3
+#float- utilizado para representar números reales. por ejemplo, 1,2, 42,42
+#boolean- utilizado para representar Verdadero o Falso.
+#complex- utilizado para representar números complejos. por ejemplo, 1,0 + 2,0j, 1,5 + 2,5j
+
+##TIPOS DE DATOS EN NUMPY##
