da2d7a8e6b4d09f66eefaa23602f699d4d1b9749
andreseguarinm <142327987+andreseguarinm@users.noreply.github.com>
2023-11-19 22:44:47 -0500

    Update Array_Indexing.py

--- SegundoCorte/Numpy/Array_Indexing.py
+++ SegundoCorte/Numpy/Array_Indexing.py
@@ -4,9 +4,9 @@
 #Por ejemplo:
 
 import numpy as np
-arr = np.array([1, 2, 3, 4])
-print(arr[0]) #Se busca el primer elemento de la matriz
-print(arr[1}) #Se busca el segundo elemento de la matriz
+arr1 = np.array([1, 2, 3, 4])
+print(arr1[0]) #Se busca el primer elemento de la matriz
+print(arr1[1}) #Se busca el segundo elemento de la matriz
 
 #Y así sucesivamente.
 
@@ -15,21 +15,16 @@ print(arr[1}) #Se busca el segundo elemento de la matriz
 #Piense en las matrices 2D como en una tabla con filas y columnas, donde la dimensión representa la fila y el índice la columna.
 #Por ejemplo:
 
-import numpy as np
-arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-print('Segundo elemento de la primera fila : ', arr[0, 1]) #Corresponde al segundo valor de la primera fila, que en este caso es "2".
-
-import numpy as np
-arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-print('5th element on 2nd row: ', arr[1, 4])
+arr2 = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('Segundo elemento de la primera fila : ', arr2[0, 1]) #Corresponde al segundo valor de la primera fila, que en este caso es "2".
+print('Quinto elemento de la segunda fila: ', arr2[1, 4])
 
 ##ACCESO A MATRICES 3 D##
 #Para acceder a elementos de matrices tridimensionales podemos utilizar enteros separados por comas que representan las dimensiones y el índice del elemento.
 #Por ejemplo:
 
-import numpy as np
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-print(arr[0, 1, 2]) #En este caso, imprimirá el número 6
+arr3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(arr3[0, 1, 2]) #En este caso, imprimirá el número 6
 
 #Esto sucede porque en el comando arr[0,1,2] nos lo indica. La matriz se compone completa tiene 2 dimensiones, las cuales son submatrices; dichas submatrices
 #tienen 2 dimensiones con otras submatrices (subsubmatrices); y cada una de estas subsubmatrices tienen 3 dimensiones en los que hay números. Lo que nos dice
@@ -39,6 +34,5 @@ print(arr[0, 1, 2]) #En este caso, imprimirá el número 6
 ##INDEXACIÓN NEGATIVA##
 #Se puede utilizar este tipo de indexación para comenzar a buscar desde el último valor ingresado en la matriz:
 
-import numpy as np
-arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-print('Last element from 2nd dim: ', arr[1, -1])
+arr4 = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('Último elemento de la segunda dimensión: ', arr4[1, -1])
